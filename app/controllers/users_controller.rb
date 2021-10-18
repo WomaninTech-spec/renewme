c48df9702a1efb5e6f92d59ffe7417cfb9d8367b
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-10-18 12:46:33 +0200

    follow user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,18 @@ class UsersController < ApplicationController
   before_action :set_user, only: :show
 
   def index
-    @users = User.where(visible: true)
+    @users = User.where.not(visible: false, confirmed_at: nil, id: current_user)
   end
 
   def show
+    @current_users_skills = UsersSkill.where(user: @user, previous_skills: false)
+    @previous_users_skills = UsersSkill.where(user: @user, previous_skills: true)
+    @experiences = Experience.where(user: current_user)
+    @followings_user = FollowingsUser.where(followee: params[:id], follower: current_user).first
+    unless @followings_user
+      @followings_user = FollowingsUser.new
+    end
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :followings_users, only: [:create, :destroy]
+  end
   namespace :my do
     resources :users, only: [:show] do
       resources :users_skills

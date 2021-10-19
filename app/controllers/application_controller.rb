aaf30139f01610d1bd000a5a056f1ccc7b91132e
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-10-19 20:30:52 +0200

    users improvements _ actiopncable for messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    attr_user = [:username, :first_name, :last_name, :email, :job_position, :role, :visible, :profile_picture]
+    attr_user = [:username, :first_name, :last_name, :email, :job_position, :about_me, :role, :visible, :profile_picture]
     devise_parameter_sanitizer.permit(:sign_up, keys: attr_user)
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: attr_user)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_121530) do
+ActiveRecord::Schema.define(version: 2021_10_19_113244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 2021_10_16_121530) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "job_position"
+    t.text "about_me"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

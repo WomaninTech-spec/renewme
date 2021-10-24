0b93f7d8d2f6fe2c109bc1434c26cb6ffae99d80
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-10-24 08:38:04 +0200

    add last articles and jobs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @users = User.where.not(visible: false, confirmed_at: nil, id: current_user, role: 1).order(created_at: :desc).limit(6)
+    @articles = Article.order(created_at: :desc).limit(6)
+    @jobs = Job.order(created_at: :desc).limit(6)
   end
 end

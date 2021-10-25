a8891bb2e13f8a62522d0f7b6433f3e4855f1e21
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-10-25 08:49:44 +0200

    lazy load skills

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,10 @@
 class Skill < ApplicationRecord
   has_many :users_skills, dependent: :destroy
   has_many :users, through: :users_skills
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [:name],
+    using: {
+      tsearch: { prefix: true }
+    }
 end

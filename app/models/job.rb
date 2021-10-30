1d161cdffe620ff882e074444a6af9a253a98f63
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-10-30 18:49:28 +0200

    trix

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ApplicationRecord
   belongs_to :user
   has_many :jobs_applications, dependent: :destroy
-
+  validates :title, :description, presence: true
   include PgSearch::Model
 
   pg_search_scope :job_search,

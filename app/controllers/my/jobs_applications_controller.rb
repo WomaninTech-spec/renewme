c566bc3bb9cea7fce8878bffe0a43bef37ce1ca9
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-11-04 16:32:59 +0100

    css changes

--- app/controllers/my/jobs_applications_controller.rb
+++ app/controllers/my/jobs_applications_controller.rb
@@ -4,6 +4,6 @@ class My::JobsApplicationsController < ApplicationController
   end
 
   def my_jobs_applicants
-    @pagy, @jobs = pagy(current_user.jobs.joins(:jobs_applications).distinct, items: 5)
+    @pagy, @jobs = pagy(current_user.jobs.order(created_at: :desc).joins(:jobs_applications).distinct, items: 5)
   end
 end

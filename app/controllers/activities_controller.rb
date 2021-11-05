2134ba22c854c1be5e3005c9b4d7dd24ad81add8
Cedric Pierrejean <pierrejeancedric@gmail.com>
2021-11-05 12:58:32 +0100

    css changes show user

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,7 @@ class ActivitiesController < ApplicationController
   def index
     @pagy, @activities = pagy(Activity.order(created_at: :desc)
                                       .joins("INNER JOIN followings_users ON activities.user_id = followings_users.followee_id")
+                                      .joins("INNER JOIN users ON activities.user_id = users.id").where("users.visible = ?", true)
                                       .where("followings_users.follower_id = ?", current_user).limit(100), items: 20)
   end
 end
